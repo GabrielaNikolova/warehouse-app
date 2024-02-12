394f4c42a835ddd78d2b62da7c15cdf4d45441b7
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2024-02-12 11:23:06 +0200

    feat: add error boundary

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Header/Header';
+import NotFound from '../NotFound/NotFound';
+import { ErrorBoundary } from 'react-error-boundary';
+import { routes } from '../../statics/routes';
 
 export default function Layout() {
     return (
         <>
-            <Header />
-            <Outlet />
+            <ErrorBoundary FallbackComponent={NotFound} onReset={() => (location.href = routes.dashboard)}>
+                {/* <ErrorBoundary hasError={false} setHasError={function (): void {}}> */}
+                <Header />
+                <Outlet />
+            </ErrorBoundary>
+            {/* </ErrorBoundary> */}
         </>
     );
 }
--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,14 +1,18 @@
 import FormButton from '../Common_components/Button/Button';
-import { useNavigate } from 'react-router-dom';
-import { routes } from '../../statics/routes';
 import { Container } from '../Common_components/Global.style';
 
-function NotFound() {
-    const navigate = useNavigate();
+interface Props {
+    error: Error;
+    resetErrorBoundary: () => void;
+}
+
+function NotFound({ error, resetErrorBoundary }: Props) {
+    console.log('Error occured', error);
     return (
         <Container>
-            <h1>Page not found!</h1>
-            <FormButton type={'button'} btnText={'Go to Login Page'} onClick={() => navigate(routes.login)} />
+            <h1>Oppps, Something went wrong! Page not found!</h1>
+            <p>Try clicking the return to web page button to reload the application. </p>
+            <FormButton type={'button'} btnText={'Return to Webpage'} onClick={resetErrorBoundary} />
         </Container>
     );
 }
